774d8bb056b6a7c892e12eecd89466f4d47f88fc
Malachy Crossan <59704927+malachycrossan@users.noreply.github.com>
2024-10-17 01:07:12 -0400

    Changed naming of 'options' to 'menu_items' & 'menu' to 'main_menu' for clarity

--- Monopoly-Project/main.c
+++ Monopoly-Project/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int roll (int dice_quantity, int dice_sides);
+int roll (int dice_quantity, int dice_sides){};
 int quit (void) {return 1;}
 int new (void) {return 0;}
 
-struct option {
+struct menu_item {
 	int (*function_call)(void);
        char description[128];	
 };
@@ -13,8 +13,8 @@ struct option {
 void main (void) {
 	printf("Welcome to Malachy Crossan's Monopoly Simulator Project\n");
 	
-	#define num_menu_options 5
-	struct option menu_options[num_menu_options] = {
+	#define num_main_menu_items 5
+	struct menu_item main_menu_items[num_main_menu_items] = {
 		{new, "New Game"},
 		//add statistics option
 		//add save to file option
@@ -23,12 +23,12 @@ void main (void) {
 	};
 	int quit_menu = 0;
 	while(quit_menu != 1) {
-		for (int i = 0; i < num_menu_options; i++)
-			printf("%d: %s\n", i, menu_options[i].description); // prints "#: Description" for all options
+		for (int i = 0; i < num_main_menu_items; i++)
+			printf("%d: %s\n", i, main_menu_items[i].description); // prints "#: Description" for all options
 		printf(">");
 		int menu_choice = 0;
 		scanf("%d", &menu_choice);
-		if (menu_choice < 0 || menu_choice >= num_menu_options) printf("Invalid option! Try again...\n");
-		else quit_menu = menu_options[menu_choice].function_call();
+		if (menu_choice < 0 || menu_choice >= num_main_menu_items) printf("Invalid option! Try again...\n");
+		else quit_menu = main_menu_items[menu_choice].function_call();
 	}
 }
