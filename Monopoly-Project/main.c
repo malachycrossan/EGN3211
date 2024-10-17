3f132a4277be499587f076dfe2335c447fcdc442
Malachy Crossan <59704927+malachycrossan@users.noreply.github.com>
2024-10-17 00:56:25 -0400

    Menu structure. Added cursor.

--- Monopoly-Project/main.c
+++ Monopoly-Project/main.c
@@ -3,7 +3,7 @@
 
 int roll (int dice_quantity, int dice_sides);
 int quit (void) {return 1;}
-int dont (void) {return 0;}
+int new (void) {return 0;}
 
 struct option {
 	int (*function_call)(void);
@@ -13,15 +13,19 @@ struct option {
 void main (void) {
 	printf("Welcome to Malachy Crossan's Monopoly Simulator Project\n");
 	
-	#define num_menu_options  2
+	#define num_menu_options 5
 	struct option menu_options[num_menu_options] = {
-		{quit, "quit"},
-		{dont, "dont"}
+		{new, "New Game"},
+		//add statistics option
+		//add save to file option
+		//add load from file option
+		{quit, "Quit"}
 	};
 	int quit_menu = 0;
 	while(quit_menu != 1) {
 		for (int i = 0; i < num_menu_options; i++)
 			printf("%d: %s\n", i, menu_options[i].description); // prints "#: Description" for all options
+		printf(">");
 		int menu_choice = 0;
 		scanf("%d", &menu_choice);
 		if (menu_choice < 0 || menu_choice >= num_menu_options) printf("Invalid option! Try again...\n");
